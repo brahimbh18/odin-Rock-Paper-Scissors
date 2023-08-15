5a790397bb846fd4a3e2df9bf86217dccb46ad36
brahimbh18 <102485344+brahimbh18@users.noreply.github.com>
2023-08-15 23:04:16 +0100

    rps button are vertical(for some reason) and about page missing

--- script.js
+++ script.js
@@ -14,9 +14,8 @@ function getComputerChoice ()
 
 function playRound(playerSelection, computerSelection) 
 {
-    console.log("your score : " + player_score + "| computer's score : " + computer_score);
-
-    document.querySelector('h3').textContent = `The computer choosed : ${computerSelection}`;
+    const h3 = document.createElement('h3');
+    h3.textContent = `The computer choosed : ${computerSelection}`;
     switch (playerSelection)
     {
         case "rock":
@@ -67,25 +66,70 @@ function playRound(playerSelection, computerSelection)
             }
     
     }
+    
 }
 
 function game(playerSelection)
 {
-   
-   
-    
+
     document.querySelector('h2').textContent = playRound(playerSelection, getComputerChoice());
     document.querySelector(".player").textContent = `Player's score : ${player_score}`;
     document.querySelector(".computer").textContent = `Computer's score : ${computer_score}`;
-   
-    if (roundCount == 5) console.log('game ended');
+    roundCount++;
+    if (roundCount == 5) {
+        
+        roundCount = 0;
+        gameEnded();
+    }
 }
+
+function gameEnded() {
+
+    
+    const container = document.querySelector('.container1');
+    container.innerHTML = "";  
+
+    const div = document.createElement("div");
+    div.classList.add("result");
+
+    console.log(player_score);
+    console.log(computer_score);
+    if (player_score < computer_score) div.textContent = "Game over! You lost!"
+    else if(player_score > computer_score) div.textContent = "Congratulations! You won!";
+    else div.textContent = "Score tied!";
+
+    
+    const div1 = document.createElement("div");
+    const playBtn = document.createElement('button');
+    const homeBtn = document.createElement('button');
+    div1.classList.add('btns');
+    playBtn.classList.add('playBtn');
+    homeBtn.classList.add('homeBtn');
+
+    playBtn.textContent = "Play again";
+    homeBtn.textContent = "Home";
+    player_score = 0;
+    computer_score = 0;
+    playBtn.addEventListener('click', () =>  loadGame());
+
+    div1.appendChild(playBtn);
+    div1.appendChild(homeBtn);
+    
+    container.appendChild(div);
+    container.appendChild(div1);
+
+    
+    
+}
+
 
 function loadGame() {
 
-    document.querySelector('.container1').remove();
+    const container1 = document.querySelector('.container1');
+    container1.innerHTML = "";
 
-    const score = document.querySelector('.score');
+    const score = document.createElement('div');
+    score.classList.add(".score")
 
     
     const player = document.createElement("div")
@@ -100,9 +144,13 @@ function loadGame() {
     score.appendChild(player);
     score.appendChild(computer);
 
-    document.querySelector('h2').textContent = "choose to start!";
+    h2 = document.createElement('h2');
+    h2.textContent = "choose to start!";
 
-    const container = document.querySelector(".container");
+    
+
+    const container = document.createElement("div");
+    container.classList.add(".container")
 
     const rock = document.createElement("div");
     const paper = document.createElement("div");
@@ -112,7 +160,7 @@ function loadGame() {
     paper.textContent = "🤚";
     scissors.textContent = "✌";
 
-    //rock.onclick = console.log('ff');
+    
     rock.addEventListener('click', () => game('rock'));
     paper.addEventListener('click', () => game('paper'));
     scissors.addEventListener('click', () => game('scissors'));
@@ -123,12 +171,18 @@ function loadGame() {
         container.appendChild(element);
     });
 
-
+    container1.appendChild(score);
+    container1.appendChild(h2);
+    container1.appendChild(container);
 }
 
 function loadAboutPage() {
-    document.querySelector('.container1').remove();
+    const container = document.querySelector('.container1');
+    container.innerHTML = "";
 
-    document.querySelector('h3').textContent = "Section not ready yet! Stay tuned."
+    const h3 = document.createElement("h3");
+    h3.textContent = "Section not ready yet! Stay tuned."
 
+
+    container.appendChild(h3);
 }
